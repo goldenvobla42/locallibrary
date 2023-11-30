4f2954f3b6279cee8bfea388f232042e07debd65
Your Name <you@example.com>
2023-11-30 12:56:42 +0200

    add owasp dep checker, add test secret value

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -25,6 +25,7 @@ if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_PASSWORD = "eurigh834uhfksjdhf" #unreal secret to test if it will be found by scanners
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
